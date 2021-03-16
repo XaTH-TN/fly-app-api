f1afce031ef6aeccec1994b7af923056f23005a6
XaTH-TN <62335962+XaTH-TN@users.noreply.github.com>
2021-03-17 02:22:20 +0700

    Update FlyApplication.java

--- fly-web/src/main/java/com/edu/fly/FlyApplication.java
+++ fly-web/src/main/java/com/edu/fly/FlyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FlyApplication {
 
     public static void main(String[] args){
+        System.out.println("Hello Flu Education");
         SpringApplication.run(FlyApplication.class, args);
     }
 }
